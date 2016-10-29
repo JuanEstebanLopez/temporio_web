bbb6cfd743a067601f619c625e9e077ba17f0d57
Esteban <jelg1994@gmail.com>
2016-10-29 15:06:08 -0500

    version 0.2

---
+++ temporio/temporio_web/models.py
@@ -0,0 +1,21 @@
+#encoding:utf-8
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Apunte (models.Model):
+    nombre = models.CharField(max_length=128)
+    tipo = models.IntegerField(default=0)
+    fecha_creacion = models.CharField(max_length=128)
+    hora_creacion = models.CharField(max_length=128)
+    recurso = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+
+    fecha_subida = models.DateField(auto_now_add=True)
+    apute_id= models.AutoField(primary_key=True)
+    codido_creador=models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return unicode("{'nombre'"+ self.nombre+"}")
